6146ce55ea9791e1adc43c7b08f27d2aa66f2559
Marwan <marwanmobark9@gmail.com>
2026-06-05 13:45:34 +0300

    Code review updates

--- src/routes/companion.js
+++ src/routes/companion.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { updateCompanionProfile, getCompanionSchedule } = require('../controllers/companionController');
 const { authenticate } = require('../middleware/authMiddleware');
+const { isCompanion } = require('../middleware/RoleMiddleware');
 
-router.use(authenticate); 
+router.use(authenticate, isCompanion); 
 router.put('/profile', updateCompanionProfile);
+router.patch('/profile', updateCompanionProfile);
 router.get('/me/schedule', getCompanionSchedule);
 module.exports = router;
--- src/routes/family.js
+++ src/routes/family.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { updateFamilyProfile } = require('../controllers/familyController');
 const { authenticate } = require('../middleware/authMiddleware');
+const { isFamily } = require('../middleware/RoleMiddleware');
 
-router.use(authenticate);
+router.use(authenticate, isFamily);
 router.put('/profile', updateFamilyProfile);
+router.patch('/profile', updateFamilyProfile);
 
 module.exports = router;
